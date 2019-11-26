678e31d3165814b1c7130bf885acb1273ad1a8f4
viktor_petukhov <viktor.s.petuhov@ya.ru>
2019-11-26 17:37:46 -0500

    fac2col

--- R/plots.R
+++ R/plots.R
@@ -1,3 +1,32 @@
+#' Factor to Color
+#' @description a utility function to translate factor into colors
+#' @export
+fac2col <- function(x,s=1,v=1,shuffle=FALSE,min.group.size=1,return.details=F,unclassified.cell.color='gray50',level.colors=NULL) {
+  nx <- names(x);
+  x <- as.factor(x);
+  if(min.group.size>1) {
+    x <- factor(x,exclude=levels(x)[unlist(tapply(rep(1,length(x)),x,length))<min.group.size])
+    x <- droplevels(x)
+  }
+  if(is.null(level.colors)) {
+    col <- rainbow(length(levels(x)),s=s,v=v);
+  } else {
+    col <- level.colors[1:length(levels(x))];
+  }
+  names(col) <- levels(x);
+
+  if(shuffle) col <- sample(col);
+
+  y <- col[as.integer(x)]; names(y) <- names(x);
+  y[is.na(y)] <- unclassified.cell.color;
+  names(y) <- nx;
+  if(return.details) {
+    return(list(colors=y,palette=col))
+  } else {
+    return(y);
+  }
+}
+
 embeddingGroupPlot <- function(plot.df, groups, geom_point_w, min.cluster.size, mark.groups, font.size, legend.title, shuffle.colors, palette, ...) {
   groups <- as.factor(groups)
 
