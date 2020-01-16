0a18b9c6669b5a8da3dabae0cf53bf37388b3449
viktor_petukhov <viktor.s.petuhov@ya.ru>
2020-01-16 10:20:41 -0500

    pblapply function

--- R/helpers.R
+++ R/helpers.R
@@ -1,2 +1,16 @@
 #' @importFrom magrittr %>% %<>% %$%
 NULL
+
+#' Parallel Lapply
+#' @description parallel, optionally verbose lapply
+#' @param n.cores number of cores to use
+#' @param progress show progress bar
+plapply <- function(..., n.cores=1, progress=F) {
+  if (progress && requireNamespace("pbapply", quietly=TRUE))
+    return(pbapply::pblapply(..., cl=n.cores))
+
+  if ((n.cores == 1) || !requireNamespace("parallel", quietly=TRUE))
+    return(lapply(...))
+
+  return(parallel::mclapply(..., mc.cores=n.cores))
+}
