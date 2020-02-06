3251e4dcfe62968dd0c8294d1f9e143d34aa1cbd
viktor_petukhov <viktor.s.petuhov@ya.ru>
2020-02-06 11:49:01 -0500

    Fixed plapply

--- R/helpers.R
+++ R/helpers.R
@@ -5,12 +5,12 @@ NULL
 #' @description parallel, optionally verbose lapply
 #' @param n.cores number of cores to use
 #' @param progress show progress bar
-plapply <- function(..., n.cores=1, progress=F) {
+plapply <- function(..., n.cores=1, progress=F, mc.preschedule=TRUE, mc.allow.recursive=TRUE) {
   if (progress && requireNamespace("pbapply", quietly=TRUE))
     return(pbapply::pblapply(..., cl=n.cores))
 
   if ((n.cores == 1) || !requireNamespace("parallel", quietly=TRUE))
     return(lapply(...))
 
-  return(parallel::mclapply(..., mc.cores=n.cores))
+  return(parallel::mclapply(..., mc.cores=n.cores, mc.preschedule=mc.preschedule, mc.allow.recursive=mc.allow.recursive))
 }
