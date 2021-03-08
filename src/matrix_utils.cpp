4ed355f709d8c86a87dcab27b456eba680742ea5
viktor_petukhov <viktor.s.petuhov@ya.ru>
2021-03-08 10:59:16 -0500

    Refactored jsDist and colSumByFac

---
+++ src/matrix_utils.cpp
@@ -0,0 +1,76 @@
+#include <RcppArmadillo.h>
+
+using namespace std;
+using namespace Rcpp;
+
+//' Jensen–Shannon distance metric (i.e. the square root of the Jensen–Shannon divergence) between the columns of a dense matrix m
+//'
+//' @param m Input matrix
+//' @param ncores integer Number of cores (default=1)
+//' @return Vectorized version of the lower triangle as an R distance object, stats::dist()
+//' @examples
+//' ex = matrix(1:9, nrow = 3, ncol = 3)
+//' jsDist(ex)
+//'
+// [[Rcpp::export]]
+arma::mat jsDist(const arma::mat& m, int ncores=1) {
+  //arma::vec d(m.n_cols*(m.n_cols-1)/2);
+  arma::mat d(m.n_cols, m.n_cols, arma::fill::zeros);
+  for(int i=0; i<(m.n_cols-1); i++) {
+    arma::vec li = log(m.col(i));
+    for(int j=i+1; j<m.n_cols; j++) {
+      arma::vec lj=log(m.col(j));
+      arma::vec ji=m.col(j)+m.col(i);
+      ji=m.col(j)%lj + m.col(i)%li - ji%(log(ji)-log(2.0));
+      double v=arma::accu(ji.elem(arma::find_finite(ji)));
+      //d[m.n_cols*i - i*(i-1)/2 + j-i]=sqrt(v/2.0);
+      d(j,i)=d(i,j)=v;
+    }
+  }
+
+  return(d);
+}
+
+//' Calculates factor-stratified sums for each column
+//'
+//' @param sY sparse matrix (dgCmatrix)
+//' @param rowSel integer factor. Note that the 0-th column will return sums for any NA values; 0 or negative values will be omitted
+//' @return Matrix
+// [[Rcpp::export]]
+arma::mat colSumByFac(SEXP sY,  SEXP rowSel) {
+  // note: trailing levels that are not mentioned in the vector will be omitted, resulting in a smaller matrix
+  // need to do this as SEXP, modify the slots on the fly
+  S4 mat(sY);
+  const arma::uvec i(( unsigned int *)INTEGER(mat.slot("i")),LENGTH(mat.slot("i")),false,true);
+  const arma::ivec dims(INTEGER(mat.slot("Dim")),LENGTH(mat.slot("Dim")),false,true);
+  const arma::ivec p(INTEGER(mat.slot("p")),LENGTH(mat.slot("p")),false,true);
+  arma::vec Y(REAL(mat.slot("x")),LENGTH(mat.slot("x")),false,true);
+
+  const arma::ivec rs=arma::ivec(INTEGER(rowSel),LENGTH(rowSel),false,true);
+
+  int ncols=p.size()-1;
+  int nlevels=0;
+  for(int j=0;j<rs.size();j++) {
+    if(rs[j]!=NA_INTEGER) {
+      if(rs[j]>nlevels) { nlevels=rs[j]; }
+    }
+  }
+  if(nlevels==0) { stop("colSumByFac(): supplied factor doesn't have any levels!"); }
+  arma::mat sumM(nlevels+1,ncols,arma::fill::zeros);
+
+  // for each gene
+  for(int g=0;g<ncols;g++) {
+    int p0=p[g]; int p1=p[g+1];
+    if(p1-p0 <1) { continue; }
+    for(int j=p0;j<p1;j++) {
+      int row=i[j];
+      int f=rs[row];
+      if(f==NA_INTEGER) {
+        sumM(0,g) += Y[j];
+      } else if(f>0) {
+        sumM(f,g) += Y[j];
+      }
+    }
+  }
+  return sumM;
+}
