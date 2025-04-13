d8a5a7b9da85a4939ca9994d04b1329d86c0fee2
evanbiederstedt <evan.biederstedt@gmail.com>
2025-04-12 21:58:12 -0400

    better to use dontrun

--- R/plots.R
+++ R/plots.R
@@ -461,10 +461,10 @@ setMethod("embeddingPlot", "ANY", function(
 #' @param reduction Reduction to use for embedding (default=NULL)
 #' @return ggplot2 object
 #' @examples
-#' # so = Seurat object
-#' # 
-#' # Call function as follows:
-#' # embeddingPlot(so, groups="seurat_clusters", reduction="umap")
+#' \dontrun{
+#' # so = example Seurat object
+#' embeddingPlot(so, groups="seurat_clusters", reduction="umap")
+#' }
 #'
 #' @name embeddingPlot
 #' @rdname embeddingPlot
